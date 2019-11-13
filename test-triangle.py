b7d82d75f3193a833e65e9cadf0f7637fd36907e
Denes Simon <symdrx@gmail.com>
2019-11-13 17:03:28 +0100

    upload

--- test-triangle.py
+++ test-triangle.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
-driver.get("https://sdoktatas.github.io/first-project/triangles.html")
+driver.get("https://sdoktatas.github.io/homework/triangles.html")
 input_a = driver.find_element_by_id("a-input")
 input_b = driver.find_element_by_id("b-input")
 input_c = driver.find_element_by_id("c-input")
